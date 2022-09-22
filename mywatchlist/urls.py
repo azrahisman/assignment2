c70dffaf784cb2b28ec13ba8af500546660770c5
Azra Batrisyia Hisman <70104051+azrahisman@users.noreply.github.com>
2022-09-22 11:22:15 +0700

    revising

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -2,11 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from mywatchlist.views import show_watchlist
+from mywatchlist.views import show_watchlist, show_html, show_xml, show_json
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_watchlist, name='show_watchlist'),
+    path('html/', show_html, name='show_html'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from mywatchlist.models import WatchlistItem
+from django.http import HttpResponse
+from django.core import serializers
 
 # views
 def show_watchlist(request):
@@ -9,3 +11,19 @@ def show_watchlist(request):
         'name': 'Azra'
     }
     return render(request, "mywatchlist.html", context)
+
+def show_html(request):
+    data_watchlist_item = WatchlistItem.objects.all()
+    context = {
+        'list_item': data_watchlist_item,
+        'name': 'Azra'
+    }
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = WatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = WatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
