5bc7a7895a5042d4ce84813b63a103a089b97571
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-04 01:54:15 +0700

    attempt

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class WatchlistItem(models.Model):
-    watched = models.CharField(max_length=50)
-    title = models.TextField()
-    rating = models.IntegerField()
+    watched = models.CharField(max_length=50) # can be changed to Boolean
+    title = models.TextField() # should be changed to Char
+    rating = models.IntegerField() # should limit for validity
     release_date =  models.DateField()
     review = models.TextField()
