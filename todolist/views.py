0ea5b3770409e38dc3e6ff3915d849566ccb34ad
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-05 10:57:24 +0700

    attempt

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from todolist.models import ItemTodolist, User
 
 # Create your views here.
 def show_todolist(request):
-    return render(request, "todolist.html")
+    data_todolist_item = ItemTodolist.objects.all()
+    user =  User(first_name='Azra', last_name='Hisman', email='azra.batrisyia11@ui.ac.id')
+    user.save()
+    context = {
+    'list_item': data_todolist_item,
+    'name': 'Azra'
+}
+    return render(request, "todolist.html", context)
